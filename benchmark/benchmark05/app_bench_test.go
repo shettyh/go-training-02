81d4edf2cb4a9e6298f06fe9f5da46f6e3810d6f
Manjunatha Shetty H <manjunatha.h@razorpay.com>
2024-03-12 10:11:55 +0530

    INIT

--- benchmark/benchmark05/app_bench_test.go
+++ benchmark/benchmark05/app_bench_test.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 )
 
-// Data structure to be accessed concurrently
 type Data struct {
-	mu    sync.RWMutex
+	mu    sync.Mutex
 	value int
 }
 
-// Function to read from the data structure using a regular Mutex
 func readWithMutex(d *Data, wg *sync.WaitGroup, result chan int) {
 	defer wg.Done()
 
@@ -22,17 +19,6 @@ func readWithMutex(d *Data, wg *sync.WaitGroup, result chan int) {
 	result <- d.value
 }
 
-// Function to read from the data structure using an RWMutex
-func readWithRWMutex(d *Data, wg *sync.WaitGroup, result chan int) {
-	defer wg.Done()
-
-	d.mu.RLock()
-	defer d.mu.RUnlock()
-
-	result <- d.value
-}
-
-// Benchmark function to compare Mutex vs RWMutex performance
 func BenchmarkMutex(b *testing.B) {
 	d := Data{value: 42}
 	var wg sync.WaitGroup
@@ -45,30 +31,4 @@ func BenchmarkMutex(b *testing.B) {
 
 	wg.Wait()
 	close(result)
-}
-
-// Benchmark function to compare Mutex vs RWMutex performance
-func BenchmarkRWMutex(b *testing.B) {
-	d := Data{value: 42}
-	var wg sync.WaitGroup
-	result := make(chan int, b.N)
-
-	for i := 0; i < b.N; i++ {
-		wg.Add(1)
-		go readWithRWMutex(&d, &wg, result)
-	}
-
-	wg.Wait()
-	close(result)
-}
-
-func main() {
-	// Run benchmarks
-	fmt.Println("Benchmarking Mutex:")
-	mutexResult := testing.Benchmark(BenchmarkMutex)
-	fmt.Println(mutexResult)
-
-	fmt.Println("Benchmarking RWMutex:")
-	rwMutexResult := testing.Benchmark(BenchmarkRWMutex)
-	fmt.Println(rwMutexResult)
 }
--- benchmark/benchmark07/app_bench_test.go
+++ benchmark/benchmark07/app_bench_test.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
 )
 
-// LargeStruct represents a large data structure
-type LargeStruct struct {
+type MyStruct struct {
 	Data [10000]int
 }
 
-// Method with value receiver
-func (ls LargeStruct) sumValue() int {
-	sum := 0
-	for _, v := range ls.Data {
-		sum += v
-	}
-	return sum
-}
-
-// Method with pointer receiver
-func (ls *LargeStruct) sumPointer() int {
+func (ls MyStruct) sumValue() int {
 	sum := 0
 	for _, v := range ls.Data {
 		sum += v
@@ -35,40 +23,17 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// Function to populate the Data field with random values
-func populateData(ls *LargeStruct) {
+func populateData(ls *MyStruct) {
 	for i := range ls.Data {
 		ls.Data[i] = rand.Intn(100)
 	}
 }
 
-// Benchmark function for method with value receiver
-func BenchmarkValueReceiver(b *testing.B) {
-	largeStruct := LargeStruct{}
-	populateData(&largeStruct)
+func BenchmarkSum(b *testing.B) {
+	myStruct := MyStruct{}
+	populateData(&myStruct)
 
 	for i := 0; i < b.N; i++ {
-		largeStruct.sumValue()
+		myStruct.sumValue()
 	}
-}
-
-// Benchmark function for method with pointer receiver
-func BenchmarkPointerReceiver(b *testing.B) {
-	largeStruct := LargeStruct{}
-	populateData(&largeStruct)
-
-	for i := 0; i < b.N; i++ {
-		(&largeStruct).sumPointer()
-	}
-}
-
-func main() {
-	// Run benchmarks
-	fmt.Println("Benchmarking ValueReceiver:")
-	valueReceiverResult := testing.Benchmark(BenchmarkValueReceiver)
-	fmt.Println(valueReceiverResult)
-
-	fmt.Println("Benchmarking PointerReceiver:")
-	pointerReceiverResult := testing.Benchmark(BenchmarkPointerReceiver)
-	fmt.Println(pointerReceiverResult)
 }
